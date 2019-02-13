f9626d624d71dfc152678335cc365b016747d643
MattLdwig <mattldwig@gmail.com>
2019-02-13 11:11:47 +0100

    Updates

---
+++ precache-manifest.5c3479f53e3b9d866e52a5c380255927.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c3d907b2f06eb08b78e",
+    "url": "/FreeCodeCamp_Random-Quotes-and-Colors/static/js/runtime~main.5c3d907b.js"
+  },
+  {
+    "revision": "ca56cca3eeb7b84518cd",
+    "url": "/FreeCodeCamp_Random-Quotes-and-Colors/static/js/main.ca56cca3.chunk.js"
+  },
+  {
+    "revision": "0ea02c00b80cf6545ed4",
+    "url": "/FreeCodeCamp_Random-Quotes-and-Colors/static/js/1.0ea02c00.chunk.js"
+  },
+  {
+    "revision": "ca56cca3eeb7b84518cd",
+    "url": "/FreeCodeCamp_Random-Quotes-and-Colors/static/css/main.3e1c1833.chunk.css"
+  },
+  {
+    "revision": "e73a1f4a3dbd1f248494d60ee52b58a7",
+    "url": "/FreeCodeCamp_Random-Quotes-and-Colors/index.html"
+  }
+];
